39373b3334585b12b518e231a781210b49b1010b
frang123 <frangiuliani41@gmail.com>
2022-10-19 20:10:24 -0300

    CRUD de Categoria

--- backend/PI/src/main/java/com/example/PI/controller/CategoriaController.java
+++ backend/PI/src/main/java/com/example/PI/controller/CategoriaController.java
@@ -1,11 +1,13 @@
 package com.example.PI.controller;
 
 import com.example.PI.entities.Categoria;
+import com.example.PI.exceptions.BadRequestException;
 import com.example.PI.service.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -14,16 +16,27 @@ public class CategoriaController {
     @Autowired
     CategoriaService categoriaService;
     @PostMapping
-    public ResponseEntity<Categoria> guardarCategoria(@RequestBody Categoria categoria){
+    public ResponseEntity<Categoria> guardarCategoria(@RequestBody Categoria categoria) throws BadRequestException {
         return ResponseEntity.ok(categoriaService.guardarCategoria(categoria));
     }
     @PutMapping
-    public ResponseEntity<Categoria> actualizarCategoria(@RequestBody Categoria categoria){
+    public ResponseEntity<Categoria> actualizarCategoria(@RequestBody Categoria categoria) throws Exception {
         return ResponseEntity.ok(categoriaService.actualizarCategoria(categoria));
     }
     @GetMapping("/{id}")
-    public ResponseEntity<Categoria> buscarxId(@PathVariable Long id ){
+    public ResponseEntity<Categoria> buscarxId(@PathVariable Long id ) throws Exception {
         Optional<Categoria> categoriaBuscada = categoriaService.buscarxId(id);
         return ResponseEntity.ok(categoriaBuscada.get());
     }
+
+    @GetMapping
+    public ResponseEntity<List<Categoria>> buscarTodas(){
+        return ResponseEntity.ok(categoriaService.buscarTodas());
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> eliminarCategoria(@PathVariable Long id){
+        categoriaService.eliminarCategoria(id);
+        return ResponseEntity.ok("Se elimino la categoria " + id );
+    }
 }
+
--- backend/PI/src/main/java/com/example/PI/service/CategoriaService.java
+++ backend/PI/src/main/java/com/example/PI/service/CategoriaService.java
@@ -1,10 +1,11 @@
 package com.example.PI.service;
 
 import com.example.PI.entities.Categoria;
+import com.example.PI.exceptions.BadRequestException;
+import com.example.PI.exceptions.ResourceNotFoundException;
 import com.example.PI.repository.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -12,21 +13,32 @@ import java.util.Optional;
 public class CategoriaService {
     @Autowired
     CategoriaRepository categoriaRepository;
-    public Categoria guardarCategoria(Categoria categoria){
-        return categoriaRepository.save(categoria);
+    public Categoria guardarCategoria(Categoria categoria)  {
+     return categoriaRepository.save(categoria);
     }
 
     public List<Categoria> buscarTodas(){
         return categoriaRepository.findAll();
     }
-    public Optional<Categoria> buscarxId(Long id){
-        return categoriaRepository.findById(id);
+    public Optional<Categoria> buscarxId(Long id)throws Exception {
+        Optional<Categoria> categoriaBuscada = categoriaRepository.findById(id);
+        if(categoriaBuscada.isPresent()){
+            return categoriaBuscada;
+        }
+        else{
+            throw new ResourceNotFoundException("No existe la categoria");
+        }
     }
     public void eliminarCategoria(Long id){
         categoriaRepository.deleteById(id);
     }
 
-    public Categoria actualizarCategoria(Categoria categoria){
-        return categoriaRepository.save(categoria);
+    public Categoria actualizarCategoria(Categoria categoria)throws Exception{
+       Optional<Categoria> categoriaBuscada = categoriaRepository.findById(categoria.getId());
+       if(categoriaBuscada.isPresent()){
+            return categoriaRepository.save(categoriaBuscada.get());
+       }else{
+           throw new ResourceNotFoundException("Categoria no existe");
+       }
     }
 }
