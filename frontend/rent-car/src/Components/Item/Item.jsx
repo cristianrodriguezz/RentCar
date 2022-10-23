5d7fd0f48e801f06bb419a517f339ae78cee634b
Cristian Rodriguez <cristiandosespigas@gmail.com>
2022-10-23 00:13:28 -0300

    Arreglando estilos

--- frontend/rent-car/src/Components/Item/Item.jsx
+++ frontend/rent-car/src/Components/Item/Item.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
+const styles = {
+  'display':'flex',
+  'flex-direction':'column',
+  'alig-content':'center',
+  'justify-content':'center',
+  'max-width':'20rem'
+}
+
 
 const Item = (props) => {
   return (
-    <div className='product'>
-        <img src={props.imagen} alt="Car" />
-        <p>{props.category}</p>
-        <h3>
-            {props.title}
-        </h3>
-        <p>{props.description}
-        <span>{props.location}</span>
-        </p>
+    <div style={{'display':'flex'}}>
+      <div style={{'width':'300px'}}>
+        <img src={props.imagen} alt="Car" style={{'width':'100%'}} />
+      </div>
+      <div style={styles}>  
+          <p>{props.category}</p>
+          <h3>
+              {props.title}
+          </h3>
+          <p>{props.description}
+          <span>{props.location}</span>
+          </p>
+      </div>
     </div>
   )
 }
