6585b175ea421104527bc93863a2f3af6d6b4b2d
frang123 <frangiuliani41@gmail.com>
2022-10-24 14:18:52 -0300

    Bloque de busqueda

--- frontend/rent-car/src/App.js
+++ frontend/rent-car/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { Routes , Route } from "react-router-dom";
 import Layout from './Components/Layout/Layout';
 import Home from './view/Home/Home';
+import Category from './view/categories/Category';
+import Car from './view/Car/Car';
 
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
       <Layout>
         <Routes>
           <Route path='/' exact element={<Home/>}/>
+          <Route path='/categories'  element={<Category/>}/>
+          <Route path='/car'  element={<Car/>}/>
         </Routes>
       </Layout>
   );
--- frontend/rent-car/src/Components/Body/Body.js
+++ frontend/rent-car/src/Components/Body/Body.js
@@ -2,10 +2,28 @@ import React from 'react'
 import './body.css'
 import data from '../../data.json'
 import Listado from '../Listado/Listado'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faCalendarDays, faClock, faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons"
+
 
 const Body = () => {
   return (
-    <div>
+    <div className='body'>
+      <div className='form'>
+      <div className='formSearchInput'>
+      <FontAwesomeIcon icon={faMagnifyingGlass} />
+      <input type='text' placeholder="What's your pick-up city or airport? " className='searchInput'>
+      </input>
+      </div>
+      <div className='formSearchInput'>
+      <FontAwesomeIcon icon={faCalendarDays} />
+      <span className='formSearchText'>date to date</span>
+      </div>
+      <div className='formSearchInput'>
+      <FontAwesomeIcon icon={faClock} />
+      <span className='formSearchText'>time to pick-up and drop-off the car</span>
+      </div>
+      </div>
       <Listado data = {data}/>
     </div>
   )
