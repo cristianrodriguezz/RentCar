803716370326c063cdb1e7b8d6586c9706b0efa1
frang123 <frangiuliani41@gmail.com>
2022-10-25 15:51:05 -0300

    final bloque buscador, body.

--- frontend/rent-car/src/view/Home/Home.jsx
+++ frontend/rent-car/src/view/Home/Home.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import Body from '../../Components/Body/Body'
 import data from '../../data.json'
 import Form from '../../Components/form/Form'
+import Listado from '../../Components/Listado/Listado'
 
 const Home = () => {
   return (
-        <Body/>
-  )
+        
+        <Body>
+        <Form/>
+        <Listado data={data}/>
+        </Body>        
+  ) 
 }
 
 export default Home
