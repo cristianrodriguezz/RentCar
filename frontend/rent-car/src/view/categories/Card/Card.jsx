0cf7374780c57f55db91d97aaedb30d19bb02c66
frang123 <frangiuliani41@gmail.com>
2022-10-26 15:33:15 -0300

    Bloque buscador completo

---
+++ frontend/rent-car/src/view/categories/Card/Card.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+const styles = {
+  'display':'flex',
+  'flexDirection':'column',
+  'gap':'1rem',
+  'aligContent':'center',
+  'justifyContent':'center',
+  'maxWidth':'20rem'
+}
+const contendor = {
+  'display':'flex',
+  'boxShadow': '10px 7px 19px -7px rgba(0,0,0,0.5)',
+  'maxWidth':'710px',
+  'maxHeight':'250px',
+  'borderRadius': '10px 10px 10px 10px',
+  'backgroundColor':'#fff',
+  'padding':'10px'
+}
+const buttonStyle = {
+  'backgroundColor': 'var(--first-color)',
+  'color':'white',
+  'border':'none',
+  'borderRadius':'5px'
+}
+
+
+const Card = (props) => {
+  return (
+    <div style={contendor}>
+      <div style={{'width':'344px','display':'flex','alignItems':'center', 'justifyContent':'center'}}>
+        <img src={props.imagen} alt="Car" style={{'width':'100%','margin':'auto'}} />
+      </div>
+      <div style={styles}>  
+          <h3>
+              {props.titulo}
+          </h3>
+          <p>{props.descripcion}
+          </p>
+          <button style={buttonStyle}>Ver detalle</button>
+      </div>
+    </div>
+  )
+}
+
+export default Card
--- frontend/rent-car/src/view/categories/Category.jsx
+++ frontend/rent-car/src/view/categories/Category.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
-
+import React, { useEffect , useState} from 'react'
+import  axios  from 'axios'
+import Card from './Card/Card'
 const Category = () => {
+    const [categorias,setCategoria] = useState([])
+    useEffect(()=>{
+      axios.get("http://localhost:8080/categorias")
+      .then(response =>{
+          setCategoria(response.data)
+      .catch(error=>{
+        console.log(error);
+      })    
+      },[])
+    })
+ 
   return (
-    <div>Category</div>
-  )
-}
+    <div>
+      {
+        categorias?.map(categoria=>
+          <Card
+            key={categoria.id}
+            titulo={categoria.titulo}
+            descripcion={categoria.descripcion}
+            imagen={categoria.urlImagen}
+          />
+          
+          )
+      }
+    </div>
+  ) }
+
 
 export default Category
