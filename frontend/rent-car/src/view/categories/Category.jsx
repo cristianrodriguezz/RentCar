84e048a898fc989da9cf3bedb1b11714389ffda1
Gino <ginobrunori17@gmail.com>
2022-10-26 16:19:14 -0300

    Subo correcion vista categories de nuevo

--- frontend/rent-car/src/view/categories/Category.jsx
+++ frontend/rent-car/src/view/categories/Category.jsx
@@ -3,17 +3,6 @@ import Listado from '../../Components/Listado/Listado';
 import data from '../../data.json';
 
 const Category = () => {
-    const [categorias,setCategoria] = useState([])
-    useEffect(()=>{
-      axios.get("http://localhost:8080/categorias")
-      .then(response =>{
-          setCategoria(response.data)
-      .catch(error=>{
-        console.log(error);
-      })    
-      },[])
-    })
- 
   return (
     <div style={{heigh: "100vh"}}>
       <Listado data={data} />
