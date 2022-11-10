152540253356725d64bfef48a5691817e2d562d4
frang123 <frangiuliani41@gmail.com>
2022-11-10 16:37:34 -0300

    cambiando galeria de imagenes

--- frontend/rent-car/src/Components/ProductoSelect/galeriaImagenes.jsx
+++ frontend/rent-car/src/Components/ProductoSelect/galeriaImagenes.jsx
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import * as React from "react";
-import "./galeriaImagenes.css";
-import { Popover } from "@mui/material";
-import { useState } from "react";
-import SwipeableTextMobileStepper from "./carouselImageFade";
-import { Context } from "../../Contexts/CategoryContextProvider";
-import { useContext,useRef } from "react";
-=======
 import * as React from 'react';
 import './galeriaImagenes.css';
 import { Popover,} from '@mui/material';
@@ -16,43 +7,12 @@ import { Context }from '../../Contexts/CategoryContextProvider'
 import { useContext} from 'react'
 import useFetch from '../../Utils/useFetch';
 import { useParams } from 'react-router';
->>>>>>> c72f094fc2975792160553f836f91e86130baf5e
 
 const ImageGridGallery = (props) => {
-  const { anchor, setAnchor } = useContext(Context);
-  const [anchorEl,setAnchorEl] = useState(null)
-  const openPopover = (event) => {
+  const {anchor, setAnchor} = useContext(Context);
+  const openPopover = () => {
     setAnchor(true);
-    setAnchorEl(event.currentTarget)
-
   };
-<<<<<<< HEAD
-  const ref = useRef(null)
-  return (
-    <div ref={ref}>
-      <CarrouselFadeGallery anchor= {anchor} anchorEl={anchorEl}/>
-      <div className="gridImageContainer" >
-        <div className="gridImageItem" id="item1">
-          {" "}
-          <img src={itemData[1].img} alt={itemData[1].title} />
-        </div>
-        <div className="gridImageItem" id="item2">
-          {" "}
-          <img src={itemData[1].img} alt={itemData[1].title} />
-        </div>
-        <div className="gridImageItem" id="item3">
-          {" "}
-          <img src={itemData[1].img} alt={itemData[1].title} />
-        </div>
-        <div className="gridImageItem" id="item4">
-          {" "}
-          <img src={itemData[1].img} alt={itemData[1].title} />
-        </div>
-        <div className="gridImageItem" id="item5">
-          <img src={itemData[1].img} alt={itemData[1].title}></img>
-          <button onClick={openPopover}>ver mas</button>
-        </div>
-=======
   const params = useParams()
   const Response = useFetch(`http://localhost:8080/productos/${params.id}`)
 
@@ -61,68 +21,76 @@ const ImageGridGallery = (props) => {
     <CarrouselFadeGallery anchor ={anchor}/>
       <div className='gridImageContainer'>
         
-        <div className='gridImageItem'  id='item1'> <img src = {Response?.imagenes[0]?.url} alt={itemData[1].title} loading/></div>
-        <div className='gridImageItem' id='item2'> <img src={itemData[1].img} alt={itemData[1].title} /></div>
-        <div className='gridImageItem'id='item3' > <img src={itemData[1].img} alt={itemData[1].title} /></div>
-        <div className='gridImageItem'id='item4'> <img src={itemData[1].img} alt={itemData[1].title} /></div>
-        <div className='gridImageItem'id='item5' >
+        <div className='gridImageItem'  id='item1'> <img src = {itemData[2].producto.imagen} alt={itemData[1].title}/></div>
+        <div className='gridImageItem' id='item2'> <img src={itemData[2].producto.imagen} alt={itemData[1].title} /></div>
+        <div className='gridImageItem'id='item3' > <img src={itemData[1].producto.imagen} alt={itemData[1].title} /></div>
+        <div className='gridImageItem'id='item4'> <img src={itemData[1].producto.imagen} alt={itemData[1].title} /></div>
+        <div className='gridImageItem'id='item5'> 
           
-        <img src={itemData[1].img} alt={itemData[1].title}></img>
+        <img src={itemData[1].producto.imagen} alt={itemData[1].title}></img>
         <button onClick={openPopover} >ver mas</button>
->>>>>>> c72f094fc2975792160553f836f91e86130baf5e
       </div>
+
+    </div>
     </div>
   );
-};
-const CarrouselFadeGallery = ({anchor,anchorEl}) => {
-
+}
+const CarrouselFadeGallery = ({anchor}) => {
   return (
     <div>
       <Popover
         open={Boolean(anchor)}
         classes={{ paper: "MuiPopover-paper" }}
-        //anchorReference="none"
-        anchorReference = "anchorEl"
-        anchorEl= {document.body}
-        //anchorPosition={{ top: 375, left: 750 }}
+        anchorReference="anchorPosition"
+        anchorPosition={{ top: 375, left: 750 }}
         anchorOrigin={{
-          vertical: "center",
-          horizontal: "center",
+          vertical: 'center',
+          horizontal: 'center',
         }}
         transformOrigin={{
-          vertical: "center",
-          horizontal: "center",
+          vertical: 'center',
+          horizontal: 'center',
         }}
-        sx={{
-          bgcolor: "#686868",
+        sx = {{
+          bgcolor: '#686868'
         }}
       >
-        <SwipeableTextMobileStepper />
+        <SwipeableTextMobileStepper/>
       </Popover>
     </div>
-  );
-};
+  );  
+
+}
 
 const itemData = [
   {
-    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
-    title: "Breakfast",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
-    title: "Burger",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
-    title: "Camera",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
-    title: "Coffee",
-  },
-  {
-    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
-    title: "Hats",
-  },
+    "id": 1,
+    "category":"Lujosos",
+    "producto":{
+        "imagen":"https://cdn.imagin.studio/getImage?&customer=arfranciscogiulianicompany&make=alfa-romeo&modelFamily=giulietta&modelRange=124-spider&modelVariant=ca&modelYear=2018&powerTrain=petrol&bodySize=2&paintId=pspc0036&angle=01",
+        "title": "Alfa Romeo Giulietta",
+        "description":"Modelo 2018, a gasolina, un auto lujoso para disfrutar.",
+        "location":"Rosario, Santa Fe, Argentina"
+    }
+},
+
+{
+    "id": 2,
+    "category":"SUV",
+    "producto":{
+        "title":"Jeep Grand Wagoneer",
+        "imagen":"https://crdms.images.consumerreports.org/c_lfill,w_720,q_auto,f_auto/prod/cars/cr/model-years/13713-2022-jeep-grand-wagoneer",
+        "description":"Modelo 2021, una SUV para ideal para las sierras.",
+        "location":" La Cumbrecita, Cordoba, Argentina"
+    }
+},
+{"id": 3,
+"category":"Familiar",
+"producto":{
+    "title":"Volkswagen Vento",
+    "imagen":"https://la-motorbit-media.s3.amazonaws.com/2016/10/volkswagen-vento-allstar-2017-2.jpg",
+    "description":"Modelo 2020, un sedan con un gran habitaculo.",
+    "location":" La Plata, Buenos Aires, Argentina"
+}}
 ];
 export default ImageGridGallery;
