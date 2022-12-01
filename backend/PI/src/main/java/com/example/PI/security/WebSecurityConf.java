b0c1be6c80bc965055b7172e633a3137fbbf9a83
frang123 <frangiuliani41@gmail.com>
2022-12-01 16:56:51 -0300

    agregando cambios al webSecurityConf y agregando archivo cargador de datos para usuario admin

--- backend/PI/src/main/java/com/example/PI/security/WebSecurityConf.java
+++ backend/PI/src/main/java/com/example/PI/security/WebSecurityConf.java
@@ -95,6 +95,7 @@ public class WebSecurityConf  extends WebSecurityConfigurerAdapter {
                 .exceptionHandling().authenticationEntryPoint(jwtEntryPointConfig).and()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.POST,"/auth/**","/usuarios/**").permitAll()
+                .antMatchers(HttpMethod.POST,"/productos", "/ciudades" , "/caracteristicas", "/categorias").hasAnyRole("ADMIN")
                 .antMatchers(HttpMethod.POST,"/reservas").hasAnyRole("USER")
                 .and()
                 .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
