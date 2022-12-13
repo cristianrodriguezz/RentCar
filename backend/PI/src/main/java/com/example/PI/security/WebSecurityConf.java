02e45760cf1a678b94faf146e657501127e84c4d
ElementaLucky <lucasyoung00@gmail.com>
2022-12-13 18:54:48 -0300

    Actualizando back

--- backend/PI/src/main/java/com/example/PI/security/WebSecurityConf.java
+++ backend/PI/src/main/java/com/example/PI/security/WebSecurityConf.java
@@ -94,9 +94,9 @@ public class WebSecurityConf  extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .exceptionHandling().authenticationEntryPoint(jwtEntryPointConfig).and()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.POST,"/auth/**").permitAll()
+                .antMatchers(HttpMethod.POST,"/auth/**","/usuarios/**").permitAll()
                 .antMatchers(HttpMethod.GET,"/productos").permitAll()
-                .antMatchers(HttpMethod.POST,"/productos", "/ciudades" , "/caracteristicas", "/categorias","/usuarios/**").hasAnyRole("ADMIN")
+                .antMatchers(HttpMethod.POST,"/productos", "/ciudades" , "/caracteristicas", "/categorias").hasAnyRole("ADMIN")
                 .antMatchers(HttpMethod.POST,"/reservas").hasAnyRole("USER")
                 .and()
                 .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
